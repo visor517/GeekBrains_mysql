7848f53ed669b7be5779312e83ff007dc4b9a909
visor517 <visor517@yandex.ru>
2021-03-22 16:07:31 +0300

    done

---
+++ lesson4/task3.sql
@@ -0,0 +1,17 @@
+USE vk;
+
+UPDATE media_types 
+SET	name = "фото"
+WHERE id = 1;
+
+UPDATE media_types 
+SET	name = "видео"
+WHERE id = 2;
+
+UPDATE media_types 
+SET	name = "аудио"
+WHERE id = 3;
+
+UPDATE media_types 
+SET	name = "документ"
+WHERE id = 4;
