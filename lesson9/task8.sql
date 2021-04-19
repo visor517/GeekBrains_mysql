5b828b581a529fa9199934bc57839d9bea111dab
visor517 <visor517@yandex.ru>
2021-04-19 20:21:09 +0300

    fixes

--- lesson9/task8.sql
+++ lesson9/task8.sql
@@ -2,35 +2,44 @@
 # С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день",
 # с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
 
-DROP PROCEDURE IF EXISTS hello;
+DROP FUNCTION IF EXISTS hello;
 
 DELIMITER //
 
-CREATE PROCEDURE hello()
+CREATE FUNCTION hello()
+RETURNS varchar(32) NO SQL 
 BEGIN
 	SET @h = HOUR(CURTIME());
-	SELECT
-		CASE 
-			WHEN @h < 6 THEN "Доброй ночи"
-			WHEN @h IN (6,7,8,9,10,11) THEN "Доброе утро"
-			WHEN @h IN (12,13,14,15,16,17) THEN "Добрый день"
-			WHEN @h >= 18 THEN "Добрый вечер"
-		END;
+	RETURN CASE
+		WHEN @h < 6 THEN "Доброй ночи"
+		WHEN @h IN (6,7,8,9,10,11) THEN "Доброе утро"
+		WHEN @h IN (12,13,14,15,16,17) THEN "Добрый день"
+		WHEN @h >= 18 THEN "Добрый вечер"
+	END;
 END //
 
 DELIMITER ;
 
-CALL hello();
+SELECT hello();
 
 #2 В таблице products есть два текстовых поля: name с названием товара и description с его описанием. Допустимо присутствие обоих полей или одно из них. Ситуация,
 # когда оба поля принимают неопределенное значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены.
 # При попытке присвоить полям NULL-значение необходимо отменить операцию.
 
-DROP TRIGGER IF EXISTS check_name_description;
+DROP TRIGGER IF EXISTS update_name_description;
+DROP TRIGGER IF EXISTS insert_name_description;
 
 DELIMITER //
 
-CREATE TRIGGER check_name_description BEFORE UPDATE ON products
+CREATE TRIGGER update_name_description BEFORE UPDATE ON products
+FOR EACH ROW
+BEGIN
+	IF NEW.name IS NULL AND NEW.description IS NULL THEN
+		SIGNAL SQLSTATE '45000' SET message_text = 'Update Canceled. И name, и description не могут быть Null';
+	END IF;
+END //
+
+CREATE TRIGGER insert_name_description BEFORE INSERT ON products
 FOR EACH ROW
 BEGIN
 	IF NEW.name IS NULL AND NEW.description IS NULL THEN
@@ -41,6 +50,7 @@ END //
 DELIMITER ;
 
 UPDATE products SET name=NULL, description=NULL WHERE id = 6;
+INSERT INTO products(name, description, price, catalog_id) VALUES (NULL, NULL, 7120.00, 1);
 
 #3 (по желанию) Напишите хранимую функцию для вычисления произвольного числа Фибоначчи. Числами Фибоначчи называется последовательность в которой число равно
 # сумме двух предыдущих чисел. Вызов функции FIBONACCI(10) должен возвращать число 55
